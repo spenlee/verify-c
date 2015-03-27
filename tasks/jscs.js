66cd3a83604d0bc5d8231760253890cc33d8cbc7
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-27 13:28:00 -0400

    Fix jscs task

--- tasks/jscs.js
+++ tasks/jscs.js
@@ -4,16 +4,28 @@ var gulp = require('gulp');
 var paths = gulp.paths;
 var plugins = gulp.plugins;
 
-function runJscs(paths) {
+function runJscs(paths, done) {
   var tasks = [];
   paths.forEach(function(path) {
-    var task = gulp.src(path)
-      .pipe(plugins.jscs());
-    tasks.push(task);
+    gulp.task(path, function() {
+      return gulp.src(path + '/**/*.js')
+        .pipe(plugins.jscs({
+          configPath: '.jscsrc'
+        }));
+    });
   });
-  return plugins.sequence(tasks);
+  return plugins.sequence(paths, done);
 }
 
 gulp.task('jscs', [], function(done) {
-  runJscs([paths.app, paths.e2e, paths.tasks]);
+  return runJscs([paths.app, paths.e2e, paths.tasks], done);
+});
+gulp.task('jscs:app', [], function(done) {
+  return runJscs([paths.app], done);
+});
+gulp.task('jscs:e2e', [], function(done) {
+  return runJscs([paths.e2e], done);
+});
+gulp.task('jscs:tasks', [], function(done) {
+  return runJscs([paths.tasks], done);
 });
