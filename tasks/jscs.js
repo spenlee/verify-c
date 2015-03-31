4cca6be66fcda1b121998a3b211d9d31bf36da8d
Ryan Brady <ryanbradynd05@gmail.com>
2015-03-31 13:28:00 -0400

    Add tasks for fixing jscs issues

--- tasks/jscs.js
+++ tasks/jscs.js
@@ -4,28 +4,49 @@ var gulp = require('gulp');
 var paths = gulp.paths;
 var plugins = gulp.plugins;
 
-function runJscs(paths, done) {
+function runJscs(paths, fix, done) {
   var tasks = [];
   paths.forEach(function(path) {
     gulp.task(path, function() {
-      return gulp.src(path + '/**/*.js')
-        .pipe(plugins.jscs({
-          configPath: '.jscsrc'
-        }));
+      if (fix) {
+        return gulp.src(path + '/**/*.js')
+          .pipe(plugins.jscs({
+            configPath: '.jscsrc',
+            fix: true
+          }))
+          .pipe(gulp.dest(path));
+      } else {
+        return gulp.src(path + '/**/*.js')
+          .pipe(plugins.jscs({
+            configPath: '.jscsrc'
+          }));
+      }
     });
   });
   return plugins.sequence(paths, done);
 }
 
 gulp.task('jscs', [], function(done) {
-  return runJscs([paths.app, paths.e2e, paths.tasks], done);
+  return runJscs([paths.app, paths.e2e, paths.tasks], false, done);
+});
+gulp.task('jscs:fix', [], function(done) {
+  return runJscs([paths.app, paths.e2e, paths.tasks], true, done);
 });
 gulp.task('jscs:app', [], function(done) {
-  return runJscs([paths.app], done);
+  return runJscs([paths.app], false, done);
+});
+gulp.task('jscs:app:fix', [], function(done) {
+  return runJscs([paths.app], true, done);
 });
 gulp.task('jscs:e2e', [], function(done) {
-  return runJscs([paths.e2e], done);
+  return runJscs([paths.e2e], false, done);
+});
+gulp.task('jscs:e2e:fix', [], function(done) {
+  return runJscs([paths.e2e], true, done);
 });
 gulp.task('jscs:tasks', [], function(done) {
-  return runJscs([paths.tasks], done);
+  return runJscs([paths.tasks], false, done);
+});
+gulp.task('jscs:tasks:fix', [], function(done) {
+  return runJscs([paths.tasks], true, done);
 });
