6b3841235eb3a1880ff22449d72c84e85eb0bfba
Ryan Brady <ryanbradynd05@gmail.com>
2015-04-05 13:28:00 -0400

    Fix jscs issue

--- tasks/clean.js
+++ tasks/clean.js
@@ -4,6 +4,6 @@ var gulp = require('gulp');
 var paths = gulp.paths;
 var plugins = gulp.plugins;
 
-gulp.task('clean', [], function (done) {
+gulp.task('clean', [], function(done) {
   plugins.del([paths.dist + '/**'], done);
 });
