c4f7594bb40f7d58346c5ef2e2d41d8dcdb4a0dc
Ryan Brady <ryanbradynd05@gmail.com>
2015-04-07 13:28:00 -0400

    Add sourcemap support

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -4,15 +4,19 @@ var gulp = require('gulp');
 var paths = gulp.paths;
 var plugins = gulp.plugins;
 
-gulp.task('scripts', ['clean', 'jscs', 'jshint'], function(done) {
+gulp.task('scripts', ['jscs', 'jshint'], function(done) {
   return gulp.src(paths.app + '/**/*.js')
+      .pipe(plugins.sourcemaps.init())
       .pipe(plugins.concat('app.js'))
+      .pipe(plugins.sourcemaps.write('.'))
       .pipe(gulp.dest(paths.dist + '/js'));
 });
 
-gulp.task('scripts:prod', ['clean', 'jscs', 'jshint'], function(done) {
+gulp.task('scripts:prod', ['jscs', 'jshint'], function(done) {
   return gulp.src(paths.app + '/**/*.js')
+      .pipe(plugins.sourcemaps.init())
       .pipe(plugins.concat('app.js'))
       .pipe(plugins.uglify())
+      .pipe(plugins.sourcemaps.write('.'))
       .pipe(gulp.dest(paths.dist + '/js'));
 });
--- tasks/style.js
+++ tasks/style.js
@@ -4,17 +4,21 @@ var gulp = require('gulp');
 var paths = gulp.paths;
 var plugins = gulp.plugins;
 
-gulp.task('style', ['clean'], function(done) {
+gulp.task('style', [], function(done) {
   return gulp.src(paths.style + '/**/*.scss')
+      .pipe(plugins.sourcemaps.init())
       .pipe(plugins.sass({outputStyle: 'nested'}).on('error', plugins.sass.logError))
       .pipe(plugins.concat('app.css'))
+      .pipe(plugins.sourcemaps.write('.'))
       .pipe(gulp.dest(paths.dist + '/css'));
 });
 
-gulp.task('style:prod', ['clean'], function(done) {
+gulp.task('style:prod', [], function(done) {
   return gulp.src(paths.style + '/**/*.scss')
+      .pipe(plugins.sourcemaps.init())
       .pipe(plugins.sass({outputStyle: 'compressed'}).on('error', plugins.sass.logError))
       .pipe(plugins.concat('app.css'))
       .pipe(plugins.minifyCss())
+      .pipe(plugins.sourcemaps.write('.'))
       .pipe(gulp.dest(paths.dist + '/css'));
 });
