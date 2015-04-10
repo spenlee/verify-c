137f13e42717acf729840a21b053bfba05ea7dac
Ryan Brady <ryanbradynd05@gmail.com>
2015-04-10 13:28:00 -0400

    Add js dependencies

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -4,7 +4,25 @@ var gulp = require('gulp');
 var paths = gulp.paths;
 var plugins = gulp.plugins;
 
+function packageDependencies(min) {
+  gulp.src([
+    (min) ? 'bower_components/angular/angular.min.js' :
+      'bower_components/angular/angular.js',
+    (min) ? 'bower_components/angular-ui-router/release/angular-ui-router.min.js' :
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+    (min) ? 'bower_components/bootstrap/dist/js/bootstrap.min.js' :
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+    (min) ? 'bower_components/jquery/dist/jquery.min.js' :
+      'bower_components/jquery/dist/jquery.js'
+  ])
+  .pipe(plugins.sourcemaps.init())
+  .pipe(plugins.concat('vendor.js'))
+  .pipe(plugins.sourcemaps.write('.'))
+  .pipe(gulp.dest(paths.dist + '/js'));
+}
+
 gulp.task('scripts', ['jscs', 'jshint'], function(done) {
+  packageDependencies(false);
   return gulp.src(paths.app + '/**/*.js')
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.concat('app.js'))
@@ -13,6 +31,7 @@ gulp.task('scripts', ['jscs', 'jshint'], function(done) {
 });
 
 gulp.task('scripts:prod', ['jscs', 'jshint'], function(done) {
+  packageDependencies(true);
   return gulp.src(paths.app + '/**/*.js')
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.concat('app.js'))
