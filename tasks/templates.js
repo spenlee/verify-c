5510b7d630c500527daff558e17b4ade7d39f228
Ryan Brady <ryanbradynd05@gmail.com>
2015-04-19 13:28:00 -0400

    Fix template caching

--- tasks/templates.js
+++ tasks/templates.js
@@ -11,6 +11,14 @@ var files = [
   '!' + paths.app + '/404.html'
 ];
 
+var options = {
+    file: 'templates.js',
+    options: {
+        module: 'app.templates',
+        standalone: true
+    }
+};
+
 function copyHtml() {
   gulp.src([paths.app + '/index.html', paths.app + '/404.html'])
     .pipe(gulp.dest(paths.dist));
@@ -19,14 +27,14 @@ function copyHtml() {
 gulp.task('templates', [], function(done) {
   copyHtml();
   return gulp.src(files)
-      .pipe(templateCache())
+      .pipe(templateCache(options.file, options.options))
       .pipe(gulp.dest(paths.dist + '/js'));
 });
 
 gulp.task('templates:prod', [], function(done) {
   copyHtml();
   return gulp.src(files)
-      .pipe(templateCache())
+      .pipe(templateCache(options.file, options.options))
       .pipe(plugins.uglify())
       .pipe(gulp.dest(paths.dist + '/js'));
 });
