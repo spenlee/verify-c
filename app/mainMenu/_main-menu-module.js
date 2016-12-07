4d080219af1077c7d15114f69ab84b5df484fb37
Ying CHEN <Emma@wirelessprv-10-194-185-97.near.illinois.edu>
2016-12-07 05:25:30 -0600

    finished login and the layout of the main menu

--- app/mainMenu/_main-menu-module.js
+++ app/mainMenu/_main-menu-module.js
@@ -10,6 +10,16 @@ angular.module('app.main-menu', [])
 
 MainViewController.$inject = ['RestService', 'ConstantsService'];
 function MainViewController(RestService, ConstantsService) {
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById('myBtn');
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName('close')[0];
+
   var vm = this;
   vm.title = 'MENU';
   RestService.getUsers()
@@ -26,4 +36,21 @@ function MainViewController(RestService, ConstantsService) {
         ConstantsService.reloadUrlToPath('#/login');
       });
   };
+  // When the user clicks on the button, open the modal 
+  vm.modal = function() {
+    modal.style.display = 'block';
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  vm.close = function() {
+    modal.style.display = 'none';
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+
 }
